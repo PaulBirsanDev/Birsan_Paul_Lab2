4b95cd8db8e637f06f2ad0c17bcb4622beef5998
Paul Bîrsan <paulbirsan15@gmail.com>
2025-11-16 10:33:07 +0200

    Add project files.

---
+++ Models/Publisher.cs
@@ -0,0 +1,11 @@
+﻿namespace Birsan_Paul_Lab2.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public ICollection<Book>? Books { get; set; } // Navigation property
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Birsan_Paul_Lab2.Data;
+using Birsan_Paul_Lab2.Models;
+
+namespace Birsan_Paul_Lab2.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Birsan_Paul_Lab2.Data.Birsan_Paul_Lab2Context _context;
+
+        public DetailsModel(Birsan_Paul_Lab2.Data.Birsan_Paul_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (book is not null)
+            {
+                Book = book;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
